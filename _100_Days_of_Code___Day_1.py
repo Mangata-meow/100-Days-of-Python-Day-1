27a248cb7e74baced0f03353500503ebd30d9aeb
julia <julia@I-LOVE-BONO>
2024-10-14 16:41:40 +0200

    Dodaj pliki projektów.

---
+++ _100_Days_of_Code___Day_1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?")
+pet = input("What's your pet's name?")
+print("Your band's name could be: " + city + " " + pet)
